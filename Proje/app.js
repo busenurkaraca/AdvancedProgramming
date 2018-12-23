d33a4d1d9f9f1b21b47f86cad532b01f69417763
Busenur <busenurkaraca97@gmail.com>
2018-12-23 11:40:31 +0300

    update

--- Proje/app.js
+++ Proje/app.js
@@ -1,5 +1,11 @@
 
   // Initialize Firebase
+  let ihtiyac_sahipleri = [];
+  let gonulluler = [];
+  let str = "";
+  let set = new Set();
+  const array_gonullu = [];
+  var newUser;
   var config = {
     apiKey: "AIzaSyCG27hKirhEFYvCR9IiCeQ9kpj5V-ChE3w",
     authDomain: "newproject-6d384.firebaseapp.com",
@@ -44,7 +50,6 @@ function add(){
 		newUser[key] = value;
 	}
 	 usersRef.push(newUser);
-
 }
 
 function add_gonullu(){
@@ -58,6 +63,52 @@ function add_gonullu(){
 		newGonullu[key] = value;
 	}
 	 usersRef.push(newGonullu);
-
+	window.alert("Yardım etme isteğiniz tarafımıza ulaşmıştır. İlginiz için çok teşekkür ederiz.")
 }
 
+function readDataUsers() {
+	//var ref = firebase.database().ref();
+	dbRef.on("value", function(snapshot) {
+		//console.log(snapshot.val());
+		var usersRef = firebase.database().ref("users/");
+		usersRef.on("child_added", function(data, prevChildKey) {
+			 newUser = data.val();
+				ihtiyac_sahipleri.push(newUser);
+				console.log(ihtiyac_sahipleri);
+				makeTable(ihtiyac_sahipleri);
+		});
+	}, function (error) {
+		console.log("Error: " + error.code);
+	});
+}
+  function readDataGonullu() {
+	  dbRef.on("value", function(snapshot) {
+		  var usersRef = firebase.database().ref("gonullu/");
+		  usersRef.on("child_added", function(data, prevChildKey) {
+			  var newGonullu = data.val();
+			  gonulluler.push(newGonullu);
+			  console.log(gonulluler);
+			  makeTable(gonulluler);
+
+		  });
+	  }, function (error) {
+		  console.log("Error: " + error.code);
+	  });
+  }
+
+  function makeTable(x){
+	  let row = "";
+	  for (let j of Object.keys(x[0]))
+		  row += "<th>"+j+"</th>";
+	  let text = "<tr>"+row+"</tr>";
+	  row="";
+	  for (let i of x ) {
+		  for(let k of Object.keys(x[0])){
+			  row += "<td>"+i[k]+"</td>";}
+		  text+= "<tr>"+row+"</tr>";
+		  row="";
+	  }
+	  out.innerText="Rows: " +(x.length+1)
+	  out.innerText+="\n Columns: " +(Object.keys(x[0]).length)
+	  tablo.innerHTML = text;
+  }
